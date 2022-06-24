d5c94788648f64eba6275b51f068eccf12282308
Hisao Tanabe <xtanabe@gmail.com>
2022-06-24 23:30:18 +0900

    14, note

--- 014/sol.cpp
+++ 014/sol.cpp
@@ -6,5 +6,14 @@ int main() {
     long long n;
     cin >> n;
     
+    for (long long i = 2; i * i <= n; i++) {
+        while (n % i == 0) {
+            n /= i;
+            cout << i << endl;
+        }
+    }
+    if (n >=2) {
+        cout << n << endl;
+    }
     return 0;
 }
